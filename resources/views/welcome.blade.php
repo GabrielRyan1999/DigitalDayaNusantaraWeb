71025316ed75a2b0066a206c300d75a1e38438bf
RyanRyanRyanRyanRyan <42666943+GabrielRyan1999@users.noreply.github.com>
2023-01-04 14:49:56 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ryan
                 </div>
 
                 <div class="links">
